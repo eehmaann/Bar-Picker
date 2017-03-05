5aac2d6d84935576955fb850faafeba9c1b41464
Eric Ehmann <eaehmann@gmail.com>
2017-03-05 18:26:36 -0500

    renamed variables

--- js/finalproject.js
+++ js/finalproject.js
@@ -1,10 +1,10 @@
 var add = document.getElementsByClassName("add");
 var hint = document.getElementsByClassName("hint");
 var edit = document.getElementsByClassName("edit");
-var dowhat;
-var barlist=[];
+var doWhat;
+var barList=[];
 
-function Bar(barnum, barname, address, city) {
+function Bar(barnum, barname, address, city){
         this.barnum=barnum;
         this.barname=barname;
         this.address=address;
@@ -22,67 +22,86 @@ window.addEventListener("load", function() {
     var name = document.getElementById("name");
     var address = document.getElementById("address");
     var city = document.getElementById("city");
-    var doit = document.getElementById("doit");
+    var doIt = document.getElementById("doIt");
     var inform = document.getElementsByClassName("info infoHead");
-    var displist=document.getElementById("output");
+    var displayArea=document.getElementById("output");
     var list=document.getElementById("thelist");
-    var ranbar=document.getElementById("randbar");
-    var num=document.getElementById("num");
-    var regname= /^[a-z]|[A-Z]/;
-    var regnum= /^\d/;
-    var regaddress =/^\d ?=[a-z]|[A-Z]/;
-    var regcity=/^[a-z]|[A-Z]/;
+    var randomBar=document.getElementById("randbar");
+    var selectionNumber=document.getElementById("selectionNumber");
+    var regName= /^[a-z]|[A-Z]/;
+    var regNum= /^\d/;
+    var regAddress =/^\d ?=[a-z]|[A-Z]/;
+    var regCity=/^[a-z]|[A-Z]/;
+
+        // when loading the check box will be unchecked
+    legal.checked=false;
+
+    if(legal) {
+        legal.addEventListener("change", function() {
+            if( legal.checked) {
+            // if the user claims to be a legal age, show the buttons
+                showButtons();
+            }
+
+            else {
+            // hide the functionality of the program
+                hideButtons();               
+            }            
+        }, false)
+    }
 
 // sets up iniitial values in the database and makes sure there is never less than 2 bars
 	if (localStorage.getItem("barKey") === null) {
-		setlist();
+		setList();
 	} 
    	
     // This function makes the text boxes change so that the user is editing the text directly, rather than writing anew
-    num.addEventListener('keyup', function() {
-        delnum=num.value-1;
+    selectionNumber.addEventListener('keyup', function() {
+        barListNumber=selectionNumber.value-1;
          barKey = JSON.parse(window.localStorage.getItem("barKey"));
-        if(barKey[delnum].barname) {
-        name.value=barKey[delnum].barname;
-        address.value=barKey[delnum].address;
-        city.value=barKey[delnum].city;
+        if(barKey[barListNumber].barname){
+        name.value=barKey[barListNumber].barname;
+        address.value=barKey[barListNumber].address;
+        city.value=barKey[barListNumber].city;
         }
     }, false );
     
     //clear the display section
-    function cleardisplay(){
+    function clearDisplay() {
 	 while (list.firstChild) {
                 list.removeChild(list.firstChild);
             }
     }   
 
 	// Displays a randomly chosen bar 
-	function dopick() {
+	function doPick() {
 	 // if a current bar was picked, it will delete that bar from the field
-                if (ranbar.firstChild) {
-                ranbar.removeChild(ranbar.firstChild);
-                }       
-                barKey = JSON.parse(window.localStorage.getItem("barKey"));
-                // pick a random number based on list of array
-                 var thepick= Math.floor(Math.random() * (barKey.length));
-                 // create Dom element fo random bar to be placed in
-                 var newDiv = document.createElement("div");
-                newDiv.setAttribute("class", "info");
-                var text=document.createTextNode("This is the bar you want:  "+barKey[thepick].barname+ " " + barKey[thepick].address + " " + barKey[thepick].city);
-                // place text
-                newDiv.appendChild(text);
-                //place div
-                ranbar.appendChild(newDiv);            
-        }
+        if (randomBar.firstChild) {
+            randomBar.removeChild(randomBar.firstChild);
+        }   
+
+        barKey = JSON.parse(window.localStorage.getItem("barKey"));
+        // pick a random number based on list of array
+        var thepick= Math.floor(Math.random() * (barKey.length));
+        // create Dom element fo random bar to be placed in
+        var newDiv = document.createElement("div");
+        newDiv.setAttribute("class", "info");
+        var text=document.createTextNode("This is the bar you want:  "+barKey[thepick].barname+ " " + barKey[thepick].address + " " + barKey[thepick].city);
+        // place text
+        newDiv.appendChild(text);
+        //place div
+        randomBar.appendChild(newDiv);            
+    }
+	
 	// Create a bar list with three bars
-	function setlist() {
+	function setList() {
         var druid = new Bar(0, "The Druid", "1357 Cambridge St.", "Cambridge");
-        barlist.push(druid);
+        barList.push(druid);
         var grendal = new Bar(1, "Grendal's Den", "89 Winthrop St.", "Cambridge");
-        barlist.push(grendal)
+        barList.push(grendal)
         var charlie = new Bar (2, "Charlie's Kitchen", "10 Eliot St.", "Cambridge");
-        barlist.push(charlie);
-        window.localStorage.setItem("barKey", JSON.stringify(barlist));
+        barList.push(charlie);
+        window.localStorage.setItem("barKey", JSON.stringify(barList));
    }
 	// creates elements in the Dom to display information about the bars
     function showBarRow(dataObject, element) {
@@ -112,187 +131,194 @@ window.addEventListener("load", function() {
             newDiv.appendChild(addressDiv);            
         }
 
-         if (dataObject.city !== 'undefined') {
+        if (dataObject.city !== 'undefined') {
             var cityDiv = document.createElement("div");
             text =document.createTextNode(dataObject.city);
             cityDiv.appendChild(text);
             newDiv.appendChild(cityDiv);            
         }
     }        
-    
-      // when loading the check box will be unchecked
-    legal.checked=false;
+   
 
-    if(legal) {
-        legal.addEventListener("change", function() {
-            if( legal.checked) {
-            // if the user claims to be a legal age, show the buttons
-        		pickBar.style.display ="inline";
-                fullList.style.display = "inline";
-                addBar.style.display="inline";
-                editBar.style.display="inline";
-                deleteBar.style.display="inline";
-                doit.style.display="block";
-
-
-            }
-            else {
-            // hide the functionality of the program
-                pickBar.style.display = "none";
-                fullList.style.display="none";
-                addBar.style.display="none";
-                editBar.style.display="none";
-                deleteBar.style.display="none";
-                removeedit();
-                removeadd();
-                cleardisplay();
-            }
-        }, false)
-    }
     if(fullList) {
         fullList.addEventListener("click", function() {
-         	cleardisplay();  
-            for (var i in inform) {
+         	clearDisplay();  
+            addInformClassName();
+            createBarList();
+        }, false)
+    }
+
+    function addInformClassName(){
+           for (var i in inform) {
                 if (inform.hasOwnProperty(i)) {
                         inform[i].className = 'info infoHead show-class';
                 }
             }
-             if(window.localStorage.getItem("barKey")) {
-            	var barKey = JSON.parse(window.localStorage.getItem("barKey"));
-            	for(var i =0; i< barKey.length; i++){
-                	var savedBar= new Bar (i+1, barKey[i].barname,barKey[i].address,barKey[i].city, barKey[i].barstate);
-                	barlist.push(savedBar);
-                	showBarRow(savedBar, thelist);
-            	}
-        	}
-        }, false)
+    }
+
+    function createBarList(){
+        if(window.localStorage.getItem("barKey")) {
+            var barKey = JSON.parse(window.localStorage.getItem("barKey"));
+            for(var i =0; i< barKey.length; i++){
+                var savedBar= new Bar (i+1, barKey[i].barname, barKey[i].address, barKey[i].city, barKey[i].barstate);
+                barList.push(savedBar);
+                showBarRow(savedBar, thelist);
+            }
+        }
     }
 
     if(addBar) {
         addBar.addEventListener("click", function() {
-            dowhat="addabar";
-            showadd();
-            removeedit();
+            doWhat="addabar";
+            showAdd();
+            removeEdit();
         }, false)
     }
 
     if(editBar) {
         editBar.addEventListener("click", function() {
-            dowhat="editabar";
-            editordelete()
-            showadd();
+            doWhat="editabar";
+            showNumberTextBox()
+            showAdd();
         }, false)
     }
-    if(deleteBar){
+
+    if(deleteBar) {
         deleteBar.addEventListener("click", function() {
-            dowhat="deleteabar";
-            editordelete()
-            removeadd();
+            doWhat="deleteabar";
+            showNumberTextBox()
+            removeAdd();
        
         }, false)
     }
+
     if(pickBar) {
         pickBar.addEventListener("click", function() {
-            dowhat="pickBar"
-            dopick();
-            removeadd();
-            removeedit();
+            doWhat="pickBar"
+            doPick();
+            removeAdd();
+            removeEdit();
         }, false)
     }
-    if(doit) {
-        doit.addEventListener("click", function() {
+
+    if(doIt) {
+        doIt.addEventListener("click", function() {
                 var barname = name.value;
                 var baraddress = address.value;
                 var barcity = city.value;
-                var delnum=num.value;    
+                var barListNumber=selectionNumber.value;    
 
-            if(dowhat=="addabar") {
+            if(doWhat=="addabar") {
             // checks to make sure all the information is set
-               if ( regname.test(barname) && regaddress.test(baraddress) && regcity.test(barcity)) {
+               if ( regName.test(barname) && regAddress.test(baraddress) && regCity.test(barcity)) {
                 	var barnum = JSON.parse(window.localStorage.getItem("barKey")).length+1;
                 	var pub = new Bar(barnum, barname, baraddress, barcity);
-                	barlist.push(pub);
+                	barList.push(pub);
                 	showBarRow(pub, output);
-                	window.localStorage.setItem("barKey", JSON.stringify(barlist));
+                	window.localStorage.setItem("barKey", JSON.stringify(barList));
                 }
+
                 else {
                 	alert("You have left some needed information out.  Please complete");
             	}
             }
-
-            else if(dowhat=="pickBar"){
-            	dopick();
+            
+            if(doWhat=="pickBar") {
+            	doPick();
             }
 
-            else if(dowhat=="deleteabar"){
+            if(doWhat=="deleteabar") {
             // makes sure that the user typed a number
             	var didDelete = false;
-            	if(regnum.test(delnum)){
-                	if(window.localStorage.getItem("barKey")){
-                 		cleardisplay();
-                    	var barKey = JSON.parse(window.localStorage.getItem("barKey"));
-                    	barlist=[];
+            	if(regNum.test(barListNumber)) {
+                 	if(window.localStorage.getItem("barKey")) {
+                 		clearDisplay();
+                     	var barKey = JSON.parse(window.localStorage.getItem("barKey"));
+                      	barList=[];
                     	for(var i =0; i< barKey.length; i++) {
-                        	if(i!=delnum-1) {
+                        	if(i!=barListNumber-1) {
                         		if(didDelete == false) {
                             		var savedBar= new Bar (i+1, barKey[i].barname,barKey[i].address,barKey[i].city, barKey[i].barstate);
                             	}
+                            
                             	else {
                             		savedBar= new Bar (i, barKey[i].barname,barKey[i].address,barKey[i].city, barKey[i].barstate);
                             	}
-                            	barlist.push(savedBar);
+
+                            	barList.push(savedBar);
                             	showBarRow(savedBar, thelist);
-                            	window.localStorage.setItem("barKey", JSON.stringify(barlist));
+                            	window.localStorage.setItem("barKey", JSON.stringify(barList));
                         	}
+                        
                         	else {
                         		didDelete = true;
                         	}
                     	}
                 	}
             	}
+            	
             	else {
             		alert("You must type the number of the bar to be deleted in the box");
             	}
             }
-            
-            else if(dowhat=="editabar") {
+
+            if(doWhat=="editabar") {
             // check to make sure all fields have information before changing to avoid
             //empty or undefined areas
-            	if(regaddress.test(baraddress) && regcity.test(barcity) && regname.test(barname) && regnum.test(delnum)) {
+            	if(regAddress.test(baraddress) && regCity.test(barcity) && regName.test(barname) && regNum.test(barListNumber)) {
                  	if(window.localStorage.getItem("barKey")) {
-                 		cleardisplay();
+                 		clearDisplay();
                      	var barKey = JSON.parse(window.localStorage.getItem("barKey"));
-                      	barlist=[];
+                      	barList=[];
                     	for(var i =0; i< barKey.length; i++) {
-                        	if(i!=delnum-1) {
+                        	if(i!=barListNumber-1) {
                             	var savedBar= new Bar (i+1, barKey[i].barname,barKey[i].address,barKey[i].city);                                
                         	}
                         	else {
-                            	savedBar= new Bar(delnum, barname, baraddress, barcity);
+                            	savedBar= new Bar(barListNumber, barname, baraddress, barcity);
                         	}
-                        	barlist.push(savedBar);
-                        	showBarRow(savedBar, thelist);
-                        	window.localStorage.setItem("barKey", JSON.stringify(barlist));
+                        barList.push(savedBar);
+                        showBarRow(savedBar, thelist);
+                        window.localStorage.setItem("barKey", JSON.stringify(barList));
                     	}
                     }
                 }
             }
             // reset all textboxes
-            num.value="";
+        	selectionNumber.value="";
             name.value="";
         	address.value="";
         	city.value="Cambridge";
+
         	if(JSON.parse(window.localStorage.getItem("barKey")).length==1) {
-        		setlist();
+        		setList();
         		alert("You deleted too many bars.  That's ok.  I'll give you three to choose from.");
         	}
         }, false)
 
     }
-})
+    function showButtons() {
+        pickBar.style.display ="inline";
+        fullList.style.display = "inline";
+        addBar.style.display="inline";
+        editBar.style.display="inline";
+        deleteBar.style.display="inline";
+        doIt.style.display="block";
+    }
 
+    function hideButtons() {
+         pickBar.style.display = "none";
+                fullList.style.display="none";
+                addBar.style.display="none";
+                editBar.style.display="none";
+                deleteBar.style.display="none";
+                removeEdit();
+                removeAdd();
+                clearDisplay();
+    }
+})
 // add the elements that would be needed for adding
-function showadd() {
+function showAdd() {
     for (var i in add) {
         if (add.hasOwnProperty(i)) {
             add[i].className = 'add show-textbox';
@@ -304,9 +330,8 @@ function showadd() {
         }
     }
 }
-
 // add the elements that relate to just editing or deleting
-function editordelete() {
+function showNumberTextBox() {
     for (var i in edit) {
          if (edit.hasOwnProperty(i)) {
             edit[i].className = 'edit show-num';
@@ -315,7 +340,7 @@ function editordelete() {
 }
 
 // remove the elements a user would need to 
-function removeadd() {
+function removeAdd() {
     for (var i in add) {
         if (add.hasOwnProperty(i)) {
             add[i].className = 'add hidden';
@@ -327,9 +352,8 @@ function removeadd() {
         }
     }
 }
-
 //remove the elements that relate to editing and deleting
-function removeedit() {
+function removeEdit() {
     for (var i in edit) {
         if (edit.hasOwnProperty(i)) {
             edit[i].className= "edit hidden";
